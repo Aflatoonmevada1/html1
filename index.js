9ae64b4d4535ded40f2a70dec2c4e7e2da8fc7e8
Aflatoonmevada1 <dmevada8@gmail.com>
2023-11-20 18:09:12 +0530

    jkghkjhjh

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const htmlDocs = require("html-docx-js");
 const fs = require("fs");
 const path = require("path");
 
-console.log("dhruv");
 const app = express();
 let htmlContent = `<p style="text-align:center">Hi<br />[[u-i-d]]<br /><strong>Lorem Ipsum&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy</strong> text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p><table align="center" border="1" cellpadding="1" cellspacing="1" style="width:500px" summary="Summary"><caption>Caption</caption><thead><tr>
 <th scope="col">Header1</th>
